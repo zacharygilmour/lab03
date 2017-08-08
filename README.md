f80fe4b6c6812c1acd34db6e4321d53fe17f0864
Rudra Saha <z5161081@ad.unsw.edu.au>
2017-08-08 11:43:47 +1000

    Fibonacci here it is

---
+++ README.md
@@ -0,0 +1,17 @@
+# week-3-starter
+Starter code for week 3 lab exercise<br>
+This lab is to be done in pairs <br>
+
+You should find three files
+<ol>
+<li> duplicate.py </li>
+<li> fibonacci.py </li>
+<li> shuffle.py </li>
+</ol>
+
+Inside these files, you need to implement a function. <br>
+The documentation for the functions are available within the respective file.<br>
+
+<strong> Note </strong> that for this lab, you need to get yourself marked within the lab<br>
+When you are done, push your changes to git, show your work to your tutor and get yourself 
+marked off.
---
+++ duplicate.py
@@ -0,0 +1,25 @@
+'''
+Write a program that accepts a sequence of whitespace separated words 
+as input and prints the words after removing all duplicate words and 
+sorting them alphanumerically. Suppose the following input is 
+supplied to the program:
+hello world and practice makes perfect and hello world again
+Then, the output should be:
+again and hello makes perfect practice world
+
+Your program should take in only one line of input.
+
+You may assume that your program will only be tested
+with valid inputs.
+'''
+
+sentence = input()
+
+
+# Define this function to return the expected output
+# Do not print it from this function
+def singlify(str):
+    # TODO
+    pass
+
+print(singlify(sentence))
